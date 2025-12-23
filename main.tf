7e91673a0cef1e1a01f4a6e7962281342adb73bb
Vishwajeetsinghh01 <vishwajeetsingh843@gmail.com>
2025-12-23 15:07:13 +0530

    Switched to lite version

--- main.tf
+++ main.tf
@@ -1,30 +1,16 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+  # We removed the AWS provider requirement to speed up the lab
 }
 
-# Configures AWS using the credentials from Jenkins
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Defines the variable we will pass from dev.tfvars
 variable "environment_name" {
   description = "The name of the environment (e.g., dev)"
   type        = string
+  default     = "dev"
 }
 
-# A simple resource that just prints a message (Free and Safe)
-resource "null_resource" "environment_check" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-
+# This is a dummy resource that finishes instantly
+resource "null_resource" "fast_check" {
   provisioner "local-exec" {
-    command = "echo 'Deploying to the ${var.environment_name} environment...'"
+    command = "echo 'Deploying to ${var.environment_name} complete!'"
   }
 }
